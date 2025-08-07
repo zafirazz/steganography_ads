9ac31a5100738870919edf067bb9f4409bbb97c5
[adishata] <jetigenovadil@gmail.com>
2025-08-07 15:04:48 +0200

    Deleted previous implementation

--- app/api/v1/stego.py
+++
@@ -1,2 +0,0 @@
-from fastapi import APIRouter, UploadFile, File
-frpm app.services.stego_service import
--- app/main.py
+++
@@ -1,6 +0,0 @@
-from fastapi import FastAPI
-from app.api.v1 import stego
-
-app = FastAPI(title="Steganography API")
-
-app.include_router(stego.router, prefix="/api/v1/stego", tags=['steganography'])
